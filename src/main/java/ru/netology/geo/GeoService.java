57e35677f9aeab54a5b88bdd51421d4240a5eede
Алексей Печин <organner@mail.ru>
2022-05-24 23:33:39 +0300

    Initial commit

---
+++ src/main/java/ru/netology/geo/GeoService.java
@@ -0,0 +1,9 @@
+package ru.netology.geo;
+
+import ru.netology.entity.Location;
+
+public interface GeoService {
+
+    Location byIp(String ip);
+    Location byCoordinates(double latitude, double longitude);
+}
---
+++ src/main/java/ru/netology/sender/MessageSender.java
@@ -0,0 +1,7 @@
+package ru.netology.sender;
+
+import java.util.Map;
+
+public interface MessageSender {
+    String send(Map<String, String> headers);
+}
